cc7ff72aec06085ad0c03325b4bc42af2edf2dc3
PVP1 <cuentadetr1@gmail.com>
2020-02-06 13:16:17 +0100

    Mi segunda edición
    Signed-off-by: PVP1 <cuentadetr1@gmail.com>

---
+++ ProyectoGitHub/src/miproyecto/Programa.java
@@ -0,0 +1,9 @@
+package miproyecto;
+
+public class Programa {
+	public static void main (String[]args) {
+		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("¿Qué tal?");
+	}
+}
